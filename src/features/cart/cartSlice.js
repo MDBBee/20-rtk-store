28315f56f8f42ff8dfa4bf0c6fcaf7f14a9b31d3
MDBBee <bobbyugbebor@gmail.com>
2024-11-21 09:47:26 +0200

    cartSlice ✅

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
+import cartItems from '../../cartItems';
 
 const initialState = {
-  cartItems: [],
+  cartItems: cartItems,
   amount: 0,
   total: 0,
   isLoading: true,
@@ -10,6 +11,42 @@ const initialState = {
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem: (state, action) => {
+      state.cartItems = state.cartItems.filter(
+        (item) => item.id !== action.payload
+      );
+    },
+    increase: (state, { payload }) => {
+      state.cartItems = state.cartItems.map((item) => {
+        if (item.id === payload) return { ...item, amount: item.amount + 1 };
+        return item;
+      });
+    },
+    decrease: (state, { payload }) => {
+      state.cartItems = state.cartItems.map((item) => {
+        if (item.id === payload) return { ...item, amount: item.amount - 1 };
+        return item;
+      });
+    },
+    calculateTotals: (state) => {
+      let total = 0;
+      let amount = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+
+      state.amount = amount;
+      state.total = total;
+    },
+  },
 });
+
+export const { clearCart, removeItem, increase, decrease, calculateTotals } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
